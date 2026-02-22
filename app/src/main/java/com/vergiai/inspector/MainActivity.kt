aa4c1fb03951b5ee350512da3f1390e4fce3e421
BaranBey1331 <152785244+BaranBey1331@users.noreply.github.com>
2026-02-22 17:56:40 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/vergiai/inspector/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.vergiai.inspector
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btnStartService)
+        val txtStatus = findViewById<TextView>(R.id.txtStatus)
+
+        btnStart.setOnClickListener {
+            if (!checkOverlayPermission()) {
+                requestOverlayPermission()
+            } else {
+                startFloatingService()
+                txtStatus.text = "Servis Çalışıyor..."
+            }
+        }
+    }
+
+    private fun startFloatingService() {
+        val intent = Intent(this, FloatingService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+    }
+
+    private fun checkOverlayPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Settings.canDrawOverlays(this)
+        } else {
+            true
+        }
+    }
+
+    private fun requestOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, 123)
+            Toast.makeText(this, "Lütfen çizim izni verin.", Toast.LENGTH_LONG).show()
+        }
+    }
+}
+
